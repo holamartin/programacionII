70a86e231cafca8c818829eaa23c6d5980185f5b
holamartin <125104644+holamartin@users.noreply.github.com>
2023-03-16 23:42:35 -0500

    Add files via upload

---
+++ Taller 1/aretriangulo.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(){
+	
+	float base, altura, area;
+	
+	printf("Ingrese la base para el triangulo\n");
+    scanf("%f",&base);
+	printf("Ingrese la altura para el triangulo\n");
+	scanf("%f",&altura);
+	
+	area=(base*altura)/2;
+	
+	hola
+	
+	printf("El area del triangulo es = %2.f", area);
+	return 0;
+}
---
+++ Taller 1/temperaturaeh.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int tempC=35;
+	int tempF;
+	
+	tempF=(tempC *9/5)+32;
+	printf("%d",tempF);
+}
